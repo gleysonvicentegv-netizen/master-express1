7669d3e368bde83f6ecf29d24984b2d11705fb61
gleysonvicentegv-netizen <gleysonvicente.gv@gmail.com>
2026-03-05 20:08:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'master-express-v3'; // Sempre que atualizar, mude o número
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png'
+];
+
+// Instalando o Service Worker e armazenando arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Ativando o SW e removendo caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Interceptando requisições e servindo arquivos do cache quando disponíveis
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
