53c014399840fd02fdae68c7845ff4fb32d5830b
gleysonvicentegv-netizen <gleysonvicente.gv@gmail.com>
2026-03-07 10:04:56 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,14 @@
-const CACHE_NAME = 'master-express-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/logo.png',
-  'https://unpkg.com/leaflet/dist/leaflet.css',
-  'https://unpkg.com/leaflet/dist/leaflet.js',
-  'https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js'
-];
+const CACHE_NAME = "master-express-v1";
+const urlsToCache = ["./","./index.html","./manifest.json","./logo.png","https://unpkg.com/leaflet/dist/leaflet.css","https://unpkg.com/leaflet/dist/leaflet.js","https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js"];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener("install", event=>{
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+self.addEventListener("fetch", event=>{
+  event.respondWith(
+    caches.match(event.request).then(response=>{
+      return response || fetch(event.request);
+    })
   );
 });
