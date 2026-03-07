d7243657b1db0acff0c7943ee874f20eb3722d2c
gleysonvicentegv-netizen <gleysonvicente.gv@gmail.com>
2026-03-07 10:12:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,42 @@
-const CACHE_NAME = "master-express-v1";
-const urlsToCache = ["./","./index.html","./manifest.json","./logo.png","https://unpkg.com/leaflet/dist/leaflet.css","https://unpkg.com/leaflet/dist/leaflet.js","https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js"];
+const CACHE_NAME = 'master-express-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.png',
+  '/manifest.json',
+  'https://unpkg.com/leaflet/dist/leaflet.css',
+  'https://unpkg.com/leaflet/dist/leaflet.js',
+  'https://cdn.jsdelivr.net/npm/@turf/turf@6/turf.min.js'
+];
 
-self.addEventListener("install", event=>{
-  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+// Instala o service worker e salva os arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(urlsToCache))
+    .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener("fetch", event=>{
+// Ativa o SW e remove caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if(key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições e retorna do cache, se possível
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response=>{
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+    .catch(() => caches.match('/index.html'))
   );
 });
